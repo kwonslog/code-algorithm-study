4d192ef93bc65b2fafb4c0d892ef9dab7b3dd824
kwonslog <korpgm@gmail.com>
2023-04-07 00:18:29 +0900

    해결

--- code-algorithm/src/com/baejoon/greedy/num2217/Main.java
+++ code-algorithm/src/com/baejoon/greedy/num2217/Main.java
@@ -1,15 +1,59 @@
 package com.baejoon.greedy.num2217;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	private void execute(int in1, int[] in2) {
+		//로프개수 k
+		//중량 w
+		//로프에 걸리는 중량은 w / k  
 
-		//문제가 이해 안되는중 ㅋㅋ.
+		//10kg. 1개의 로프를 사용하면  10kg 중량을 up
+		//10kg. 15kg 2개의 로프를 사용하면 10kg 로프에 맞춰서 20kg 중량을 up
+		//	10kg 기준 20
+		//  15kg 기준 1
+		//10kg. 15kg 25kg 3개의 로프를 사용시
+	    //	10kg 기준 30
+		//	15kg 기준 30
+		//  25kg 기준 25
+		//10kg. 15kg 25kg 45kg 4개의 로프를 사용시
+		//	10kg 기준 40
+		//	15kg 기준 45
+		//	25kg 기준 50
+		//  45kg 기준 45
+
+		//1 2 3 4 5
+		//1 * 5 = 5
+		//2 * 4 = 8
+		//3 * 3 = 9
+		//4 * 2 = 8
+		//5 * 1 = 5
+
+		//1 2 3 3 4 5
+		//1 * 6 = 6
+		//2 * 5 = 10
+		//3 * 4 = 12
+		//3 * 3 = 9
+		//4 * 2 = 8
+		//5 * 1 = 5
+
+		Arrays.sort(in2);
+
+		int t1 = in1;
+		int max = 0;
+		for( int i=0; i<in1; i++ ) {
+			 int cl = in2[i] * t1--;
+			 if( max < cl ) {
+				max = cl;
+			 }
+		}
+
+		System.out.println(max);
+		
 	}
 
 	public void solution() throws Exception {
